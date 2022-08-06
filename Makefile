73f18c1c7931d79e20f73e1f9407fb435daa4ede
rubemlrm <rubemmota89@gmail.com>
2022-08-06 20:17:04 +0100

    feat(http-client): Added basic client (#8)
    Added basic client that will act as proxy to calls.
    Added step into ci cd to run tests
    Closes Create http client to call fogos api #5
    
    Signed-off-by: Rubem Mota <rubemmota89@gmail.com>

---
+++ Makefile
@@ -0,0 +1,14 @@
+.PHONY: help
+# Display all commands
+help: # Show this help.
+	@awk '/^#/{c=substr($$0,3);next}c&&/^[[:alpha:]][[:alnum:]_-]+:/{print substr($$1,1,index($$1,":")),c}1{c=0}' $(MAKEFILE_LIST) | column -s: -t
+
+.PHONY: generate-mocks
+# Generate mocks
+generate-mocks:
+	grep -rl  internal/ -e 'type \s*[a-zA-Z0-9]\+\s* interface' | xargs -n 1 dirname | uniq | xargs -P 4 -I % sh -c 'mockery -name "[a-zA-Z0-9]*" -note "Run \`make generate-mocks\` to regenerate this file." -outpkg mocks -dir "%" -output "%/mocks"'
+
+.PHONY: tests
+# Run golang tests
+tests:
+	go test -v ./...
---
+++ internal/client/client_test.go
@@ -0,0 +1,29 @@
+package client_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Rubemlrm/fogospt-golang-client/internal/client"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClientCallWithSuccess(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	}))
+	defer server.Close()
+
+	req, err := client.Do(server.URL)
+	assert.NoError(t, err)
+	assert.Equal(t, 200, req.StatusCode)
+}
+
+func TestClientCallWith500Error(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(500)
+	}))
+	defer server.Close()
+	_, err := client.Do(server.URL)
+	assert.EqualError(t, err, "Server error")
+}
